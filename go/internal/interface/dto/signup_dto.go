13762da6b6e0cdf40cf640ce42af602620b52f07
takuma-yamaguchi <yamataku0807@gmail.com>
2025-09-16 14:47:26 +0900

    フォーマット

--- go/internal/interface/dto/signup_dto.go
+++ go/internal/interface/dto/signup_dto.go
@@ -3,12 +3,12 @@ package dto
 // SignupRequest はユーザ登録の入力DTOです。
 // email と password を受け取り、成功時はアクセストークンを返します。
 type SignupRequest struct {
-    Email    string `json:"email"`
-    Password string `json:"password"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 // SignupResponse はユーザ登録の出力DTOです。
 // 最小構成ではアクセストークンのみ返却します。
 type SignupResponse struct {
-    AccessToken string `json:"access_token"`
+	AccessToken string `json:"access_token"`
 }
